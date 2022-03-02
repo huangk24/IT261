36fa262cfe85ebe72ff62664ad0f556f660c568c
huangk24 <37856562+huangk24@users.noreply.github.com>
2022-03-02 15:03:20 -0800

    week 8

--- weeks/week8/includes/footer.php
+++ weeks/week8/includes/footer.php
@@ -1,6 +1,20 @@
 <footer>
-    <p>This is my footer</p>
-
+    <div class="inner">
+        <ul>
+            <li>Copyright <?= date('Y') ?></li>
+            <li>All Rights Reserved</li>
+            <li><a href="../../index.php">Web Design by Kai</li>
+            <li><a href="">HTML VALID</a></li>
+            <li><a href="">CSS VALID</a></li>
+        </ul>
+    </div>
 </footer>
+<?php
+//release web server resources
+@mysqli_free_result($result);
+
+//close connection to mysql
+@mysqli_close($iConn);
+?>
 </body>
 </html>
